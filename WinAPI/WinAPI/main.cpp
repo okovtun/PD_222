57ee400c366177d502c03ab43bad6014d30db42c
Clayman <Clayman@DESKTOP-DM8E77U>
2023-02-26 14:45:55 +0300

    Login message DONE

--- WinAPI/WinAPI/main.cpp
+++ WinAPI/WinAPI/main.cpp
@@ -1,6 +1,10 @@
 #include<Windows.h>
 #include"resource.h"
 
+CONST CHAR g_sz_DEFAULT_LOFIN_MESSAGE[] = "Введите имя пользователя";
+//g - Global
+//sz - String Zero
+
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
 /*
@@ -38,11 +42,34 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	{
 		HICON hIcon = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_ICON1));
 		SendMessage(hwnd, WM_SETICON, 0, (LPARAM)hIcon);
+
+		HWND hEditLogin = GetDlgItem(hwnd, IDC_EDIT_LOGIN);
+		SendMessage(hEditLogin, WM_SETTEXT, 0, (LPARAM)g_sz_DEFAULT_LOFIN_MESSAGE);
 	}
 	break;
 	case WM_COMMAND:	//Обрабатывает сообщения нажатия на кнопки и т.д.
 		switch (LOWORD(wParam))
 		{
+		case IDC_EDIT_LOGIN:
+			if (HIWORD(wParam) == EN_SETFOCUS)
+			{
+				CONST INT SIZE = 256;
+				CHAR sz_buffer[SIZE] = {};
+				HWND hEditLogin = GetDlgItem(hwnd, IDC_EDIT_LOGIN);
+				SendMessage(hEditLogin, WM_GETTEXT, SIZE, (LPARAM)sz_buffer);
+				if (strcmp(sz_buffer, g_sz_DEFAULT_LOFIN_MESSAGE) == 0)
+					SendMessage(hEditLogin, WM_SETTEXT, 0, (LPARAM)"");
+			}
+			if (HIWORD(wParam) == EN_KILLFOCUS)
+			{
+				CONST INT SIZE = 256;
+				CHAR sz_buffer[SIZE] = {};
+				HWND hEdit = GetDlgItem(hwnd, IDC_EDIT_LOGIN);
+				SendMessage(hEdit, WM_GETTEXT, SIZE, (LPARAM)sz_buffer);
+				if (strcmp(sz_buffer, "") == 0)
+					SendMessage(hEdit, WM_SETTEXT, 0, (LPARAM)g_sz_DEFAULT_LOFIN_MESSAGE);
+			}
+			break;
 		case IDC_BUTTON_COPY:
 		{
 			HWND hEditLogin = GetDlgItem(hwnd, IDC_EDIT_LOGIN);
@@ -52,7 +79,7 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 			SendMessage(hEditLogin, WM_GETTEXT, SIZE, (LPARAM)sz_buffer);
 			SendMessage(hEditPassword, WM_SETTEXT, 0, (LPARAM)sz_buffer);
 		}
-			break;
+		break;
 		case IDOK:		MessageBox(hwnd, "Была нажата кнопка OK", "Info", MB_OK | MB_ICONINFORMATION); break;
 		case IDCANCEL:	EndDialog(hwnd, 0); break;
 		}
